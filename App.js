4afdbe2eb8d1b11d8403a4cff44a38a49b119b16
Buwa <bubay565@yahoo.com>
2017-12-27 15:28:40 +0000

    added react redux dependecies and created Store/RootReducer

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+import Store from './src/Store'
 import { Header } from './src/components'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Header />
-      </View>
+      <Provider store={Store}>
+        <View style={styles.container}>
+          <Header />
+        </View>
+      </Provider>
     );
   }
 }
